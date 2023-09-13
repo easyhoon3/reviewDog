69b82a20c78134486ec851a64536791d630de813
이지훈 <jeehoon.lee3@cj.net>
2023-09-13 20:04:24 +0900

    test

---
+++ src/main/kotlin/com/group/libraryapp/service/user/FailTest.kt
@@ -0,0 +1,11 @@
+package com.group.libraryapp.service.user
+
+class FailTest {
+
+
+
+    fun testtest( a:Int) {}
+
+    fun maxLineWarning_1234567890_1234567890_1234567890_1234567890_1234567890_1234567890_1234567890_1234567890_1234567890( a:Int) = Unit
+
+}
